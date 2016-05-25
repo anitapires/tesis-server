e6d6c1675db0919c1a67cabefab3f4dd7d2713c2
Andres Cimadamore <andrescimadamore@gmail.com>
2016-05-25 19:16:12 -0300

    added Procfile & config.ru

--- app.rb
+++ app.rb
@@ -6,14 +6,16 @@ require_relative 'models/drawing'
 require_relative 'models/section'
 
 
-configure :production, :test do
-  set :bind, '0.0.0.0'
-end
-
-get '/drawings' do
-  json Drawing.all
-end
-
-post '/drawings' do
-  json message: "TODO"
+class App < Sinatra::Base
+  configure :production, :test do
+    set :bind, '0.0.0.0'
+  end
+  
+  get '/drawings' do
+    json Drawing.all
+  end
+  
+  post '/drawings' do
+    json message: "TODO"
+  end
 end
