6db13a2ece2ab83da123af387f4c3bfba6803351
Andres Cimadamore <andrescimadamore@gmail.com>
2016-06-27 23:51:52 -0300

    changed database to postgres

--- Gemfile
+++ Gemfile
@@ -4,5 +4,7 @@ gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'sinatra-activerecord'
 #gem 'sqlite3'
+gem "sinatra-cross_origin", "~> 0.3.1"
+
 
 gem 'pg'
--- app.rb
+++ app.rb
@@ -1,21 +1,40 @@
 require 'sinatra'
-require 'sinatra/activerecord'
+#require 'sinatra/activerecord'
 require 'sinatra/json'
 
-require_relative 'models/drawing'
-require_relative 'models/section'
+#require_relative 'models/drawing'
+#require_relative 'models/section'
 
 
 class App < Sinatra::Base
+
   configure :production, :test do
     set :bind, '0.0.0.0'
   end
-  
+
+
+  before do
+    content_type :json    
+    headers 'Access-Control-Allow-Origin' => '*', 
+           'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST'],
+           'Access-Control-Allow-Headers' => 'Content-Type'  
+  end
+ 
+  set :protection, false  
+
+  get '/' do
+    "Hello World"
+  end
+
+  options '/drawings' do
+    
+  end
+
   get '/drawings' do
     json Drawing.all
   end
   
   post '/drawings' do
-    json message: "TODO"
+    puts "Hola #{params}!"
   end
 end
